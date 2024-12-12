d2ce5e7f3a8d95e3b09cf2078fa26ff173d3f1fe
fallm <mlfall@umw.edu>
2024-12-12 17:20:39 -0600

    new commit

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "taschedulerapp.onrender.com"
+    "taschedulerapp.onrender.com",
+    "127.0.0.1",
 ]
 
 
@@ -87,8 +88,12 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'PORT': os.environ.get('DB_PORT'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST')
     }
 }
 
