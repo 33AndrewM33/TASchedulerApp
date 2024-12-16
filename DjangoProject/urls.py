0f6e6980d00ebb7eef87c0bc1bed6fa6989f2f54
Andrew Meder <adm@iastate.edu>
2024-12-15 18:58:03 -0600

    Admin: TA to course and section

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -24,14 +24,16 @@ urlpatterns = [
     path('home/managecourse/create/', views.create_course, name='create_course'),
     path('home/managecourse/edit/<str:course_id>/', views.edit_course, name='edit_course'),
     path('home/managecourse/delete/<str:course_id>/', views.delete_course, name='delete_course'),
-    path('home/managecourse/assign/<str:course_id>/', views.assign_instructors_to_course,
-         name='assign_instructors_to_course'),
+    path('home/managecourse/assign/<str:course_id>/', views.assign_instructors_to_course, name='assign_instructors_to_course'),
+    path('home/managecourse/assign-tas/<str:course_id>/', views.assign_tas_to_course, name='assign_tas_to_course'),
 
     # Section Management
     path('home/managesection/', views.manage_section, name='manage_section'),
     path('home/managesection/create/', views.create_section, name='create_section'),
     path('home/managesection/edit/<int:section_id>/', views.edit_section, name='edit_section'),
     path('home/managesection/delete/<int:section_id>/', views.delete_section, name='delete_section'),
+    path('home/instructor/assign_ta_to_section/<int:section_id>/', views.assign_ta_to_section, name='assign_ta_to_section_admin'),
+    path('unassign_ta/<int:section_id>/<int:ta_id>/', views.unassign_ta, name='unassign_ta'),
 
     # Account Management
     path('home/accountmanagement/', views.account_management, name='account_management'),
